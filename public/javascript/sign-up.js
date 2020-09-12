3a45c201984aa8e31abee2917b8b80c52482360b
Taylor Nyquist <tnyquist@gmail.com>
2020-09-12 18:30:23 -0500

    updated sign-up

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -1,8 +1,20 @@
-// //Date form Modal
-// $("#inputBirthday").datepicker({
-//     minDate: 1
-//   });
+const signupSeed = {
+    firstName: "John",
+    lastName: "Smith",
+    email: "johnsmith@gmail.com",
+    password: "hashedPassword",
+    gender: "male",
+    heightFeet: "5",
+    heightInches: "11",
+    birthday: "01/02/25"
+};
 
-  $( function() {
-    $( "#inputBirthday" ).datepicker();
-  } );
+const healthLogSeed = {
+    weight: 175,
+    bloodPressure: "120/80",
+    heartRate: 65,
+    exerciseDuration: 30,
+    exerciseType: "running",
+    waterDrank: 32,
+    comments: "I feel great!"
+};
