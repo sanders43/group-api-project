06e7d189b1210851e11cb6a409955cb80e967192
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-09-13 20:23:44 -0500

    blood pressure chart updated

---
+++ public/javascript/heartRate.js
@@ -0,0 +1,50 @@
+
+const xdate = [];
+const yheartRate = []
+
+makeChart();
+  
+async function makeChart(){
+  await getHR()
+  const ctx = document.getElementById('hrChart').getContext('2d');
+
+
+  const myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+      labels: xdate,
+      datasets: [{
+          label: 'Heart Rate',
+          data: yheartRate,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+              
+          borderWidth: 1
+           }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      suggestedMin: 50,
+                      suggestedMax: 80
+                  }
+              }]
+          }
+      }
+  });
+}
+
+async function getHR(){
+      const response = await fetch('/api/posts');
+      const data = await response.json();
+
+  
+      data.forEach(data => {
+          const heartRate = data.heart_rate;
+          yheartRate.push(heartRate);
+
+          const date = data.created_at;
+          xdate.push(date);
+      })
+}
