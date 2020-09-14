08aff1b2afcb666782e6732d290678329eedeec2
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-09-14 16:33:57 -0500

    dashboard feature added and homepage updated

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,92 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection')
+const { Post, User, } = require('../models')
+
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    console.log(req.session);
+    Post.findAll({
+      attributes: [
+        'id',
+        'weight',
+        'systolic_blood_pressure',
+        'diastolic_blood_pressure',
+        'heart_rate',
+        'exercise_duration',
+        'exercise_type',
+        'water_consumed',
+        'emoji_feeling',
+        'comments',
+        'created_at'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['first_name', 'last_name']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass a single post object into the homepage template
+        res.render('dashboard', {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+
+// GET single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'weight',
+        'systolic_blood_pressure',
+        'diastolic_blood_pressure',
+        'heart_rate',
+        'exercise_duration',
+        'exercise_type',
+        'water_consumed',
+        'emoji_feeling',
+        'comments',
+        'created_at'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['first_name', 'last_name']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', {
+          post,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  module.exports = router;
