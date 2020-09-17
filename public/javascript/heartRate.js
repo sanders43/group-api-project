c681336bc9bdbd67242e4a9813ffa4d79f61e4dc
Taylor Nyquist <tnyquist@gmail.com>
2020-09-16 20:32:52 -0500

    ready for master branch

--- public/javascript/heartRate.js
+++ public/javascript/heartRate.js
@@ -1,50 +1,63 @@
 const xdate = [];
 const yheartRate = []
 
+const id = document.querySelector('#user_id').innerHTML.trim();
+console.log(id);
+
 makeChart();
-  
-async function makeChart(){
-  await getHR()
-  const ctx = document.getElementById('hrChart').getContext('2d');
+
+async function makeChart() {
+
+    await getHR()
+    const ctx = document.getElementById('hrChart').getContext('2d');
 
 
-  const myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-      labels: xdate,
-      datasets: [{
-          label: 'Heart Rate',
-          data: yheartRate,
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgba(255, 99, 132, 1)',
-              
-          borderWidth: 1
-           }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      suggestedMin: 50,
-                      suggestedMax: 80
-                  }
-              }]
-          }
-      }
-  });
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xdate,
+            datasets: [{
+                label: 'Heart Rate',
+                data: yheartRate,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        suggestedMin: 50,
+                        suggestedMax: 80
+                    }
+                }]
+            }
+        }
+    });
 }
 
-async function getHR(){
-      const response = await fetch('/api/posts');
-      const data = await response.json();
+async function getHR() {
 
-  
-      data.forEach(data => {
-          const heartRate = data.heart_rate;
-          yheartRate.push(heartRate);
+    const response = await fetch(`/api/posts`, {
+        method: 'GET',
+        where: {
+            user_id: id
+        },
+        headers: { 'Content-Type': 'application/json' }
+    });
+    console.log(response);
 
-          const date = data.created_at;
-          xdate.push(date);
-      })
-}
+    const data = await response.json();
+
+    data.forEach(data => {
+        const heartRate = data.heart_rate;
+        yheartRate.push(heartRate);
+
+        const date = data.created_at;
+        xdate.push(date);
+    }
+    )
+};
 
