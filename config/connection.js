06d320d4302c43dbaa1fa4b2a84113f0ef1554dc
Waves <64664814+sanders43@users.noreply.github.com>
2020-09-20 17:21:42 -0500

    host

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 // create connection to our database, pass in your MySQL information for username and password.
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: "localhost",
+    host: "127.0.0.1",
     dialect: "mysql",
     port: 3306
 });
--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use(routes);
 
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
